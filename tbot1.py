96cf28fd73bf0ab2cff5f3b09256f2272afe1f2a
Mohammadrezaasan <108104864+Mohammadrezaasan@users.noreply.github.com>
2022-07-14 08:36:30 -0700

    add source code

---
+++ tbot1.py
@@ -0,0 +1,21 @@
+from email import message
+from email.message import Message
+from re import A
+from turtle import title
+import telebot
+bot = telebot.TeleBot("")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.reply_to(message, '''Hello\nwelcome to the crypto-and-stock-market-alert bot\nAvailable options\n \nOptions available in the future\n''')
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+	bot.reply_to(message, '''Popular questions list ?\n''')
+
+
+
+
+
+bot.infinity_polling()
+

6616ae60df16ed8ad98e0d209c673da44fba5a7a
sobhan shokri <xd3vman@gmail.com>
2022-07-14 21:56:21 +0430

    add price command
    we can get price cryptocurrencies with price command

--- tbot1.py
+++ tbot1.py
@@ -1,6 +1,7 @@
 import telebot
+import requests
 bot = telebot.TeleBot("")
-
+api_price_url = "https://api.nobitex.ir/v2/trades/"
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, '''Hello\nwelcome to the crypto-and-stock-market-alert bot\nAvailable options\n \nOptions available in the future\n''')
@@ -10,6 +11,19 @@ def send_welcome(message):
 	bot.reply_to(message, '''Popular questions list ?\n''')
 
 
+# get price from nobitex api
+@bot.message_handler(commands=['price'])
+def send_welcome(message):
+	symbol_in = message.text.replace("/price ","")
+	
+
+	response = requests.get(api_price_url+symbol_in)
+	if response.status_code == 200 :
+		response = response.json()
+		
+		tmp =  + f'{symbol_in} PRICE : ',response['trades'][0]['price']
+		bot.reply_to(message,str(tmp).strip("()"))
+	else :
+		bot.reply_to(message, "server error . try again...")
 
 bot.infinity_polling()
-

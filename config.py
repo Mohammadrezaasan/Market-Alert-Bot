758c18eb2e70ff2eb4f488297d31d71d11358644
Mohammadrezaasan <mohammadrezaasan7@gmail.com>
2022-09-12 04:10:31 -0700

    Update config.py
    1-Add config info

--- config.py
+++ config.py
@@ -1,6 +1,39 @@
-Token = ""
+Bot_Token = "Token" 
+'------------------------------------------------------------------------------------------------------------------------------------'
+RapidAPI_Key = "Token"
+'----------------------------------------------------------------------------------------------------------------------------------'
+crypto_info = {
+    'what is cryptocurrency? 🤔' :'What Is Cryptocurrency?\ncryptocurrency is a digital or virtual currency that is secured by cryptography, which makes it nearly impossible to counterfeit or double-spend. Many cryptocurrencies are decentralized networks based on blockchain technology—a distributed ledger enforced by a disparate network of computers. A defining feature of cryptocurrencies is that they are generally not issued by any central authority, rendering them theoretically immune to government interference or manipulation.',
+    'how does cryptocurrency work? 🤯' :'How does cryptocurrency work?\nCryptocurrencies run on a distributed public ledger called blockchain, a record of all transactions updated and held by currency holders.Units of cryptocurrency are created through a process called mining, which involves using computer power to solve complicated mathematical problems that generate coins. Users can also buy the currencies from brokers, then store and spend them using cryptographic wallets.If you own cryptocurrency, you don’t own anything tangible. What you own is a key that allows you to move a record or a unit of measure from one person to another without a trusted third party.Although Bitcoin has been around since 2009, cryptocurrencies and applications of blockchain technology are still emerging in financial terms, and more uses are expected in the future. Transactions including bonds, stocks, and other financial assets could eventually be traded using the technology.',
+    'how to buy cryptocurrency? 😎' : 'How to buy cryptocurrency?\n1- Do thorough research on crypto to better understand crypto.\n2- Choose a reliable exchange where you can buy cryptocurrencies.\n3- After choosing an exchange and creating an account in it, you can deposit the amount of money you want to invest in cryptocurrencies into your account.\n4- To invest in currencies, be sure to do a thorough research on the currency you want to invest in.\n5- And finally, to store your currencies, you can use the digital wallet that you have in your exchange account or use different digital wallets that you install as an application on your device(before using the wallet do enough research on it).',
+    'how to donate? 🤑' : 'How to donate?\n1- Select your desired currency, for example (BTC).\n2- Enter the wallet address to which you want to deposit.\n3- Make sure that the currency network, for example (TRC 20), is similar to the currency network you want to deposit.\n4- Finally, choose the amount of currency you want to deposit and complete the confirmation process.'
+}
+'------------------------------------------------------------------------------------------------------------------------------------'
+Addresses = {
+    'donation btc(btc)💸' :"🔴 Address :\nbc1qwsnghp2nmqytejnf0xv2tlse5hfg3uzpal3868",
+    'donation eth(eth)💸' :"🔴 Address :\n0x7B075A1c07e05d3542c13e56A61b63Da91125171",
+    'donation bnb(bnb)💸' :"🔴 Address :\nbnb16m655nnxxj8vdv08tj74u3v7uazkspqyjnam5r",
+    'donation xrp(xrp)\n💸' :"🔴 Address :\nrnqe3zNxe8nkJhDQNyBBnGmVJw7NVmi2dB",
+    'donation busd(eth)\n💸' :"🔴 Address :\n0x7B075A1c07e05d3542c13e56A61b63Da91125171",
+    'donation usdt(eth)\n💸' :"🔴 Address :\n0x7B075A1c07e05d3542c13e56A61b63Da91125171",
+    'donation usdc(matic)\n💸' :"🔴 Address :\n0x7B075A1c07e05d3542c13e56A61b63Da91125171"
+    }
+photos ={
+    'donation btc(btc)💸' : open("C:\\Users\\****\\OneDrive\\Desktop\\btc.jpg",'rb'),
+    'donation ETH(ETH)💸' : open("C:\\Users\\****\\OneDrive\\Desktop\\eth.jpg",'rb'),
+    'donation bnb(bnb)💸' : open("C:\\Users\\****\\OneDrive\\Desktop\\bnb.jpg",'rb'),
+    'donation xrp(xrp)\n💸' : open("C:\\Users\\****\\OneDrive\\Desktop\\xrp.jpg",'rb'),
+    'donation busd(eth)\n💸' : open("C:\\Users\\****\\OneDrive\\Desktop\\busd.jpg",'rb'),
+    'donation usdt(eth)\n💸' : open("C:\\Users\\****\\OneDrive\\Desktop\\usdt-eth network.jpg",'rb'),
+    'donation usdc(matic)\n💸' : open("C:\\Users\\****\\OneDrive\\Desktop\\usdc.jpg",'rb')
+    }
+'------------------------------------------------------------------------------------------------------------------------------------'
+Bot_Info = {
+    '🤖 introducing the robot 🤖': "Hello, my name is market alert bot 🤖\nI can tell you the current price of major currencies in the crypto market with just a few simple clicks 🚀\nAnd I can also check with the transaction hash whether your transfer was successful  ✅or not❌. Just a few simple clicks and enter your transaction hash, that's it.\nAnd you can use the information list 📒 to find the right answer to your questions\nAnd if you don't find your answer in that list, you can contact us 📬with just one click 🖱",
+    '🗂 if you need more information 🗂 \n📨 contact us 📨': '🟢 WhatsApp : +989906678506\n🔵 Telegram : @marketalertbotsupport\n📧 Email : cryptoandstockmarketalert@gmail.com',
+    '🗝 click here to find out what each keyword does 🗝': "🤖 Introducing the robot 🤖 :\nWith the help of this key 🗝 , you can recognize the robot and a little explanation is given about the robot.\n🗂 if you need more information 🗂 \n📨 connecte us 📨 :\nThis key shows you the ways to communicate with us 📡\n📓 click here to open the list of keywords for you 📓 : \nIt will show you a list of keywords 🗒\n💸 donation 💸 : With the help of this key, a list of available currencies for donation will be opened, and by clicking on the desired currency, you will receive its address and QR code.\nReturn to the previous page 🔙 : You can go back to the previous page using this key .\nReturn to main page ↩️ : You can return to the main page using this key.\n💵 Cryptocurrency price 💵 : By clicking on this button, a list will open for you in which two options will be displayed for you, you can choose one according to your desired currency and then click on the desired option from the two options. Choose one. Click on the currency you want and a list of available currencies will open for you to request a price. Click on any of them and you will get the price of the currency you want.\n📒 List of information 📒 : When you click on this button, a list of information will open for you, and you will get the answer to your question by clicking on the question you want.\n👨🏻‍💻 tx_hash check(ERC20) 👨🏻‍💻 : By using this key and following the instructions, you can enter your address and check it and make sure that the operation is done correctly.\n💸 send ETH(ERC20) 💸 : coming soon .... "
+}
+'------------------------------------------------------------------------------------------------------------------------------------'
 
-Kovan_api = "https://kovan.infura.io/v3/"
 
-Public_key = ""
-Private_key = ""
+

bf8472f534aa443bc290557b3ee6b599ebcf2018
Mohammad Reza Asan <mohammadrezaasan7@gmail.com>
2023-02-16 17:53:55 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-Bot_Token = "Token" 
-'------------------------------------------------------------------------------------------------------------------------------------'
-RapidAPI_Key = "Token"
-'----------------------------------------------------------------------------------------------------------------------------------'
-crypto_info = {
-    'what is cryptocurrency? 🤔' :'What Is Cryptocurrency?\ncryptocurrency is a digital or virtual currency that is secured by cryptography, which makes it nearly impossible to counterfeit or double-spend. Many cryptocurrencies are decentralized networks based on blockchain technology—a distributed ledger enforced by a disparate network of computers. A defining feature of cryptocurrencies is that they are generally not issued by any central authority, rendering them theoretically immune to government interference or manipulation.',
-    'how does cryptocurrency work? 🤯' :'How does cryptocurrency work?\nCryptocurrencies run on a distributed public ledger called blockchain, a record of all transactions updated and held by currency holders.Units of cryptocurrency are created through a process called mining, which involves using computer power to solve complicated mathematical problems that generate coins. Users can also buy the currencies from brokers, then store and spend them using cryptographic wallets.If you own cryptocurrency, you don’t own anything tangible. What you own is a key that allows you to move a record or a unit of measure from one person to another without a trusted third party.Although Bitcoin has been around since 2009, cryptocurrencies and applications of blockchain technology are still emerging in financial terms, and more uses are expected in the future. Transactions including bonds, stocks, and other financial assets could eventually be traded using the technology.',
-    'how to buBot_Token = "TOKEN" 
+Bot_Token = "TOKEN" 
 
 '------------------------------------------------------------------------------------------------------------------------------------'
 crypto_info = {
@@ -16,8 +9,8 @@ crypto_info = {
 
 '------------------------------------------------------------------------------------------------------------------------------------'
 Bot_Info = {
-    '🤖 introducing the robot 🤖': "Hello, my name is Live Information Bot 🤖\nI can tell you the stock prices of international companies and in addition to stocks, I can also tell you the prices of cryptocurrencies and when you get their prices, you can click on the (latest news) button to get the latest news about that company or cryptocurrency.\n And I can check if the crypto transactions you made were successful.".title(),
-    '🗂 if you need more information 🗂 \n📨 contact us 📨': '📧 Email : \nLive_Information_Bot@Live_Information.co',   
+    '🤖 introducing the robot 🤖': "Hello, my name is Yahoo Finance Bot 🤖\nI can tell you the stock prices of international companies and in addition to stocks, I can also tell you the prices of cryptocurrencies and when you get their prices, you can click on the (latest news) button to get the latest news about that company or cryptocurrency.\n And I can check if the crypto transactions you made were successful.".title(),
+    '🗂 if you need more information 🗂 \n📨 contact us 📨': '📧 Email : \nYahoo_Finance_Bot@Live_Information.co',   
 }
 '------------------------------------------------------------------------------------------------------------------------------------'
 bot_kaywords = ("📊 mtock market price 📊\nWith the help of this key and the instructions that have been explained, you can get the stock price of your favorite company live\n--------------------------------------------------------------------\n💵 Cryptocurrency price 💵\nWith the help of this key and the instructions explained, you can get the price of your favorite cryptocurrency live.\n--------------------------------------------------------------------\n📒 List of information 📒\nA list of questions has been prepared for those who need more information, By clicking on the desired question, you will receive the answer to that question.\n--------------------------------------------------------------------\n👨🏻‍💻 Tx_Hash Check 👨🏻‍💻\nThis key will help you to check if the transaction you made was successful or not. It is designed for two types of networks,{TRON(TRC20),Ethereum(ERC20)}.\n--------------------------------------------------------------------")
@@ -128,36 +121,5 @@ def info_stock(stock_name) :
 
     except :
         d = ''
-
-
-y cryptocurrency? 😎' : 'How to buy cryptocurrency?\n1- Do thorough research on crypto to better understand crypto.\n2- Choose a reliable exchange where you can buy cryptocurrencies.\n3- After choosing an exchange and creating an account in it, you can deposit the amount of money you want to invest in cryptocurrencies into your account.\n4- To invest in currencies, be sure to do a thorough research on the currency you want to invest in.\n5- And finally, to store your currencies, you can use the digital wallet that you have in your exchange account or use different digital wallets that you install as an application on your device(before using the wallet do enough research on it).',
-    'how to donate? 🤑' : 'How to donate?\n1- Select your desired currency, for example (BTC).\n2- Enter the wallet address to which you want to deposit.\n3- Make sure that the currency network, for example (TRC 20), is similar to the currency network you want to deposit.\n4- Finally, choose the amount of currency you want to deposit and complete the confirmation process.'
-}
-'------------------------------------------------------------------------------------------------------------------------------------'
-Addresses = {
-    'donation btc(btc)💸' :"🔴 Address :\nbc1qwsnghp2nmqytejnf0xv2tlse5hfg3uzpal3868",
-    'donation eth(eth)💸' :"🔴 Address :\n0x7B075A1c07e05d3542c13e56A61b63Da91125171",
-    'donation bnb(bnb)💸' :"🔴 Address :\nbnb16m655nnxxj8vdv08tj74u3v7uazkspqyjnam5r",
-    'donation xrp(xrp)\n💸' :"🔴 Address :\nrnqe3zNxe8nkJhDQNyBBnGmVJw7NVmi2dB",
-    'donation busd(eth)\n💸' :"🔴 Address :\n0x7B075A1c07e05d3542c13e56A61b63Da91125171",
-    'donation usdt(eth)\n💸' :"🔴 Address :\n0x7B075A1c07e05d3542c13e56A61b63Da91125171",
-    'donation usdc(matic)\n💸' :"🔴 Address :\n0x7B075A1c07e05d3542c13e56A61b63Da91125171"
-    }
-photos ={
-    'donation btc(btc)💸' : open("donation_photos\\btc.jpg",'rb'),
-    'donation ETH(ETH)💸' : open("donation_photos\\eth.jpg",'rb'),
-    'donation bnb(bnb)💸' : open("donation_photos\\bnb.jpg",'rb'),
-    'donation xrp(xrp)\n💸' : open("donation_photos\\xrp.jpg",'rb'),
-    'donation busd(eth)\n💸' : open("donation_photos\\busd.jpg",'rb'),
-    'donation usdt(eth)\n💸' : open("donation_photos\\usdt-eth network.jpg",'rb'),
-    'donation usdc(matic)\n💸' : open("donation_photos\\usdc.jpg",'rb')
-    }
-'------------------------------------------------------------------------------------------------------------------------------------'
-Bot_Info = {
-    '🤖 introducing the robot 🤖': "Hello, my name is market alert bot 🤖\nI can tell you the current price of major currencies in the crypto market with just a few simple clicks 🚀\nAnd I can also check with the transaction hash whether your transfer was successful  ✅or not❌. Just a few simple clicks and enter your transaction hash, that's it.\nAnd you can use the information list 📒 to find the right answer to your questions\nAnd if you don't find your answer in that list, you can contact us 📬with just one click 🖱",
-    '🗂 if you need more information 🗂 \n📨 contact us 📨': '🟢 WhatsApp : +989906678506\n🔵 Telegram : @marketalertbotsupport\n📧 Email : cryptoandstockmarketalert@gmail.com',
-}
-'------------------------------------------------------------------------------------------------------------------------------------'
-
 
 
